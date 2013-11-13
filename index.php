951ffd7cdc3cb557794605f2d0cd02842a140a32
William Loosman <william.wl@live.nl>
2013-11-13 12:19:01 +0100

    starting on first build

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 	<head>
 		<title>PaperBoat Webapp</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<meta name="action-url" content="http://192.168.0.83:8080/webapp.js/action.php">
+		<meta name="action-url" content="http://localhost:8080/webapp.js/action.php">
 		<meta name="action-start" action="start" />
-		<link rel="stylesheet" href="http://192.168.0.83:8080/webapp.js/css/jquery-ui.css" type="text/css">
-		<link rel="stylesheet" href="http://192.168.0.83:8080/webapp.js/css/changeofui.css" type="text/css">
-		<link rel="stylesheet" href="http://192.168.0.83:8080/webapp.js/style.css" type="text/css">
-		<script type="text/javascript" src="http://192.168.0.83:8080/webapp.js/js"></script>
+		<link rel="stylesheet" href="http://localhost:8080/webapp.js/css/jquery-ui.css" type="text/css">
+		<link rel="stylesheet" href="http://localhost:8080/webapp.js/css/changeofui.css" type="text/css">
+		<link rel="stylesheet" href="http://localhost:8080/webapp.js/style.css" type="text/css">
+		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/jquery-2.0.3.min.js"></script>
+		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/jquery-ui.js"></script>
+		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/webapp.js"></script>
 	</head>
 	<body>
-		
+		<button class="btn" action="start" level="start">test</button>
 	</body>
 </html>
