447c5125023012a754d1242dd1b08ca7290ae780
William Loosman <william.wl@live.nl>
2014-09-10 16:46:24 +0200

    renamed main file

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 	<head>
 		<title>PaperBoat Webapp 0.0.8</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<meta name="action-url" content="http://localhost:8080/webapp.js/action.php">
-		<link rel="stylesheet" href="http://localhost:8080/webapp.js/css/jquery-ui.css" type="text/css">
-		<link rel="stylesheet" href="http://localhost:8080/webapp.js/css/changeofui.css" type="text/css">
-		<link rel="stylesheet" href="http://localhost:8080/webapp.js/css/codeHighlight/github.css" type="text/css">
-		<link rel="stylesheet" href="http://localhost:8080/webapp.js/style.css" type="text/css">
-		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/jquery.js"></script>
-		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/jquery-ui.js"></script>
-		<script type="text/javascript" src="http://localhost:8080/webapp.js/js/highlight.pack.js"></script>
-		<script type="text/javascript" src="http://localhost:8080/webapp.js/webapp.js"></script>
+		<meta name="action-url" content="action.php">
+		<link rel="stylesheet" href="css/jquery-ui.css" type="text/css">
+		<link rel="stylesheet" href="css/changeofui.css" type="text/css">
+		<link rel="stylesheet" href="css/codeHighlight/github.css" type="text/css">
+		<link rel="stylesheet" href="style.css" type="text/css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/jquery-ui.js"></script>
+		<script type="text/javascript" src="js/highlight.pack.js"></script>
+		<script type="text/javascript" src="MicoBoatWebapp.js"></script>
 		<script>
 			$(document).ajaxStop(function() {
 				hljs.configure({tabReplace: '    '});
